495a484b13ea1ff2d7dd7c4bf514e76367c42422
Shriharsh <shriharsh.dev@gmail.com>
2023-12-28 16:10:14 +0530

    Modified app.js

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const libRouter = require("./routes/library")
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// add database
+mongoose.set("strictQuery",false)
+const mongodb = "mongodb+srv://shriharsh:1234dev@cluster0.amdu5ma.mongodb.net/library?retryWrites=true&w=majority"
+main().catch((err)=>console.log(err))
+async function main(){
+  await mongoose.connect(mongodb)
+}
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +28,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/library", libRouter)
+app.use("/",(req,res) => {
+  res.render("index",{title:"Welcome to the library."})
+})
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
